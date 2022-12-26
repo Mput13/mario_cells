b228bcad615a96c354e2ba64b9ca47dc5845190d
Mput13 <mputilovskij@gmail.com>
2022-12-26 20:23:59 +0500

    добавил ассетов

--- main.py
+++ main.py
@@ -8,6 +8,35 @@ FPS = 50
 WIDTH = 400
 HEIGHT = 300
 
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Box('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     # если файл не существует, то выходим
@@ -102,14 +131,23 @@ class Player(Character):
     def move(self):
         pass
 
+# группы спрайтов
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+boxes_group = pygame.sprite.Group()
 
-class Texture(pygame.sprite.Sprite):
-    image = load_image("textures/mario.png")
+tile_images = {}
+player_image = load_image('characters/mario.png')
+tile_width = tile_height = 50
 
-    def __init__(self, *group):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite. Это очень важно !!!
-        super().__init__(*group)
-        self.image = Texture.image
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
 
 
 class Camera:
@@ -130,7 +168,4 @@ class Camera:
         self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
 
 
-characters = pygame.sprite.Group()
-Character(*characters)
-textures = pygame.sprite.Group()
-Texture(*textures)
+
