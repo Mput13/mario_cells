ba3dc4dc3ebd825a1e8655f062bbc497627d2d66
Mput13 <mputilovskij@gmail.com>
2023-01-07 15:57:47 +0500

    тепербь кривовато, но работает редактор уровней, можно бегать, работает графитация

--- level_work.py
+++ level_work.py
@@ -1,16 +1,13 @@
-from tiles import Tile
+from tiles import Tile, Player
 
 
 def load_level(filename):
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
-
-    max_width = max(map(len, level_map))
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+    return level_map
 
 
 def generate_level(level):
-    new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '#':
@@ -18,5 +15,5 @@ def generate_level(level):
             elif level[y][x] == '.':
                 Tile('brick', x, y)
             elif level[y][x] == '@':
-                pass
-    return new_player, x, y
+                player = Player(x, y)
+    return player
--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import pygame
 from camera import Camera
 from constants import GRAVITY, WIDTH, HEIGHT, FPS
 from level_work import generate_level, load_level
+from sprite_groups import all_sprites, tiles_group, boxes_group
 from utils import load_image
 
 
 class Game:
     def __init__(self):
-        self.player = Player()
         self.running = False
         self.background = load_image("background.png")
         self.background = pygame.transform.scale(self.background, (WIDTH, HEIGHT))
         self.event_handlers = collections.defaultdict(list)
+        self.player = None
 
     def setup(self):
         pass
@@ -25,18 +26,17 @@ class Game:
     def start(self):
         screen = pygame.display.set_mode((WIDTH, HEIGHT))
         camera = Camera()
-        player, level_x, level_y = generate_level(load_level('test_level.txt'))
+        self.player = generate_level(load_level('test_level.txt'))
         self.running = True
         timer = pygame.time.Clock()
         self.running = True
-
         while self.running:
-            delta_t = timer.tick(FPS) / 1000
             key = pygame.key.get_pressed()
+            delta_t = timer.tick(FPS) / 1000
             if key[pygame.K_a]:
-                player.move(delta_t, left=True)
-            elif key[pygame.K_d]:
-                player.move(delta_t)
+                self.player.move(delta_t, left=True)
+            if key[pygame.K_d]:
+                self.player.move(delta_t, right=True)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
@@ -44,38 +44,13 @@ class Game:
                     for callback in callbacks:
                         callback(event)
             screen.blit(self.background, (0, 0))
+            all_sprites.draw(screen)
             self.update(screen, delta_t)
             pygame.display.flip()
 
     def update(self, surface, delta_t):
-        pass
-
-
-class Character(pygame.sprite.Sprite):
-    def __init__(self, *group, speed=200, health=100):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite. Это очень важно !!!
-        super().__init__(*group)
-        self.rect = self.image.get_rect()
-        self.x_speed = speed
-        self.y_speed = -GRAVITY
-        self.health = health
-
-
-class Player:
-    def bow_shot(self):
-        pass
-
-    def blade_shot(self):
-        pass
-
-    def shild_shot(self):
-        pass
-
-    def move(self, delta_t, left=False, right=False):
-        if right:
-            self.rect.x += self.x_speed * delta_t
-        elif left:
-            self.rect.x -= self.x_speed * delta_t
+        if not pygame.sprite.spritecollideany(self.player, tiles_group):
+            self.player.move(delta_t, down=True)
 
 
 game = Game()
